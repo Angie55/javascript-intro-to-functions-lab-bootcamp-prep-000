284993e86e72e7bac6d7ce6bad598bca1562bce7
Angela Stead <angie_stead55@yahoo.com>
2019-02-02 15:36:58 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string)
+{
+return string.toUpperCase();
+
+}
+
+function whisper(string)
+{
+return string.toLowerCase();
+
+}
+
+function logShout(string)
+{
+console.log (string.toUpperCase());
+}
+logShout('Hello');
+
+function logWhisper(string)
+{
+console.log (string.toLowerCase());
+}
+logWhisper('Hello');
+
+function sayHiToGrandma(string)
+{
+   return 'I can\'t hear you!';
+
+   
+   
+   
+  
+  
+}
